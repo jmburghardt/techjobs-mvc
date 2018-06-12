fb6c43576291a91b40bc3f67cf4ddba6b5a474ee
Joe Burghardt <jmburghardt@gmail.com>
2018-06-11 19:29:21 -0500

    done! maybe? (needs to pass test) update: first test failed. switched searchType, searchTerm ln 38 search controller. will test again.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -35,7 +35,7 @@ public class SearchController {
             return "search";
 
         } else {
-            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchTerm, searchType);
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
             model.addAttribute("jobs", jobs);
             return "search";
 
