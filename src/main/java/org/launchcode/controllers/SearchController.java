7323a617bff30013940ed49d1682dd8f3d1dc517
Joe Burghardt <jmburghardt@gmail.com>
2018-06-11 19:10:21 -0500

    done! maybe? (needs to pass test)

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -23,5 +24,36 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+        model.addAttribute("columns", ListController.columnChoices);
+        if (searchType.equals("all")) {
 
-}
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "search";
+
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchTerm, searchType);
+            model.addAttribute("jobs", jobs);
+            return "search";
+
+        }
+
+
+            /*for (HashMap<String, String> row : allJobs) {
+
+                for (String key : row.keySet()) {
+                    String aValue = row.get(key);
+
+                    if (aValue.toLowerCase().contains(searchTerm.toLowerCase())) {
+                        allJobs.add(row);
+
+                        // Finding one field in a job that matches is sufficient
+                        break;
+                    }*/
+
+
+                }
+            }
